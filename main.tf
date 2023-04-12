ad00f52578e9b171c459ccb51d34cbd09d373cf8
andrej988 <andrej.jagar@gmail.com>
2023-04-12 18:40:37 +0200

    Changed version limits

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 4.62.0"
+      version = ">= 4.62.0"
     }
     archive = {
       source = "hashicorp/archive"
-      version = "~> 2.3.0"
+      version = ">= 2.3.0"
     }
   }
-  required_version = "~> 1.0"
+  required_version = ">= 1.0"
 }
