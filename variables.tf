68b2498a7f673d5fa5afe15b232fec984864b19d
andrej988 <andrej.jagar@gmail.com>
2023-04-13 12:19:28 +0200

    Added variable types and flag if they are sensitive

--- variables.tf
+++ variables.tf
@@ -1,14 +1,35 @@
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-variable "AWS_REGION" {}
-variable "AWS_CERTIFICATE_ARN" {}
-variable "DOMAIN_NAME" {}
-variable "ENV" {}
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+  sensitive = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+  sensitive = true
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "AWS_CERTIFICATE_ARN" {
+  type = string
+}
+
+variable "DOMAIN_NAME" {
+  type = string
+}
+
+variable "ENV" {
+  type = string
+}
 
 variable "app_name" {
+  type = string
   default = "weight-tracker"
 }
 
 variable "TF_LOG" {
+  type = string
   default = "DEBUG"
 }
