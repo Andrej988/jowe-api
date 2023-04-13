16ab752e4fceff2dc94b9a3259350b3aaeb4ef18
andrej988 <andrej.jagar@gmail.com>
2023-04-13 09:57:18 +0200

    Renamed variable CERTIFICATE_ARN to AWS_CERTIFICATE_ARN

--- terraform/modules/api/domain-name.tf
+++ terraform/modules/api/domain-name.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_domain_name" "custom_domain_name" {
   domain_name              = var.DOMAIN_NAME
-  regional_certificate_arn = var.CERTIFICATE_ARN
+  regional_certificate_arn = var.AWS_CERTIFICATE_ARN
 
   endpoint_configuration {
     types = ["REGIONAL"]
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
+variable "AWS_REGION" {}
+variable "AWS_CERTIFICATE_ARN" {}
+variable "DOMAIN_NAME" {}
+variable "ENV" {}
 variable "TERRAFORM_CLOUD_ORGANIZATION" {}
 variable "TERRAFORM_CLOUD_WORKSPACE" {}
-variable "ENV" {}
-variable "AWS_REGION" {}
-variable "DOMAIN_NAME" {}
-variable "CERTIFICATE_ARN" {}
 
 variable "app_name" {
   default = "weight-tracker"
