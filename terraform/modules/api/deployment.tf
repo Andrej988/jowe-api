2b661d5d3a87c47565f90657a87ca67eb053259d
Andrej <andrej.jagar@gmail.com>
2023-04-20 17:48:48 +0200

    Fixed depends on for API Gateway deployment (#13)

--- terraform/modules/api/deployment.tf
+++ terraform/modules/api/deployment.tf
@@ -10,7 +10,26 @@ resource "aws_api_gateway_deployment" "health_tracker_api_deployment" {
   }
 
   depends_on = [
-    aws_api_gateway_rest_api.health_tracker_api
+    aws_api_gateway_rest_api.health_tracker_api,
+
+    # Measurements Endpoint
+    aws_api_gateway_resource.measurements_resource,
+    aws_api_gateway_method.options_measurements_method,
+    aws_api_gateway_integration.options_measurements_integration,
+    aws_api_gateway_method.get_measurements_method,
+    aws_api_gateway_integration.get_measurements_integration,
+    aws_api_gateway_method.post_measurements_method,
+    aws_api_gateway_integration.post_measurements_integration,
+
+    # Measurements/Id Endpoint
+    aws_api_gateway_resource.measurements_id_resource,
+    aws_api_gateway_method.options_measurements_id_method,
+    aws_api_gateway_integration.options_measurements_id_integration,
+    aws_api_gateway_method.get_measurements_id_method,
+    aws_api_gateway_integration.get_measurements_id_integration,
+    aws_api_gateway_method.delete_measurements_id_method,
+    aws_api_gateway_integration.delete_measurements_id_integration,
+
   ]
 }
 
