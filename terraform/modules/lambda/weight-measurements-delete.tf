f87d599e1b48b53a7be0afbdab1f997a576a1ca4
andrej988 <andrej.jagar@gmail.com>
2023-05-08 13:32:48 +0200

    Added locals and lambda cloudwatch log groups

--- terraform/modules/lambda/weight-measurements-delete.tf
+++ terraform/modules/lambda/weight-measurements-delete.tf
@@ -1,26 +1,36 @@
 data "archive_file" "weight_measurements_delete_zip" {
   type        = "zip"
-  source_dir  = "./lambdas/functions/weight/measurements/delete/"
+  source_dir  = "${local.weight_measurements_lambdas_directory}/delete/"
   output_path = "./temp/weight_measurements_delete.zip"
+}
+
+resource "aws_cloudwatch_log_group" "weight_measurements_delete_lambda_log_group" {
+  name = "${local.cloudwatch_lambdas_log_group_prefix}${local.weight_measurements_delete_lambda_name}"
+
+  retention_in_days = local.cloudwatch_lambdas_log_retention_in_days
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+  }
 }
 
 resource "aws_lambda_function" "weight_measurements_delete_lambda" {
   filename      = data.archive_file.weight_measurements_delete_zip.output_path
-  function_name = var.ENV == "dev" ? "${var.app_name}-api-weight-measurements-delete-dev" : "${var.app_name}-api-weight-measurements-delete"
+  function_name = local.weight_measurements_delete_lambda_name
   role          = aws_iam_role.jowe_api_lambda_crud.arn
   handler       = "index.handler"
 
   source_code_hash = data.archive_file.weight_measurements_delete_zip.output_base64sha256
-
-  runtime = "nodejs18.x"
+  runtime          = local.lambdas_common_runtime
 
   environment {
     variables = {
-      TABLE_NAME = var.dynamodb_tables["weight_measurements"]
+      TABLE_NAME = local.weight_measurements_table_name
     }
   }
 
-  layers = [ 
+  layers = [
     aws_lambda_layer_version.common_layer.arn,
     aws_lambda_layer_version.weight_measurements_shared_layer.arn,
   ]
@@ -36,5 +46,6 @@ resource "aws_lambda_function" "weight_measurements_delete_lambda" {
     aws_iam_role_policy_attachment.jowe_api_lambda_crud_AWSLambdaBasicExecutionRole_attachment,
     aws_iam_role_policy_attachment.jowe_api_lambda_dynamodb_crud_role_attachment_measurements,
     aws_lambda_layer_version.weight_measurements_shared_layer,
+    aws_cloudwatch_log_group.weight_measurements_delete_lambda_log_group,
   ]
 }
--- terraform/modules/lambda/weight-measurements-insert.tf
+++ terraform/modules/lambda/weight-measurements-insert.tf
@@ -1,28 +1,38 @@
 data "archive_file" "weight_measurements_insert_zip" {
   type        = "zip"
-  source_dir  = "./lambdas/functions/weight/measurements/insert/"
+  source_dir  = "${local.weight_measurements_lambdas_directory}/insert/"
   output_path = "./temp/weight_measurements_insert.zip"
+}
+
+resource "aws_cloudwatch_log_group" "weight_measurements_insert_lambda_log_group" {
+  name = "${local.cloudwatch_lambdas_log_group_prefix}${local.weight_measurements_insert_lambda_name}"
+
+  retention_in_days = local.cloudwatch_lambdas_log_retention_in_days
+
+  lifecycle {
+    create_before_destroy = true
+    prevent_destroy       = false
+  }
 }
 
 resource "aws_lambda_function" "weight_measurements_insert_lambda" {
   filename      = data.archive_file.weight_measurements_insert_zip.output_path
-  function_name = var.ENV == "dev" ? "${var.app_name}-api-weight-measurements-insert-dev" : "${var.app_name}-api-weight-measurements-insert"
+  function_name = local.weight_measurements_insert_lambda_name
   role          = aws_iam_role.jowe_api_lambda_crud.arn
   handler       = "index.handler"
 
   source_code_hash = data.archive_file.weight_measurements_insert_zip.output_base64sha256
-
-  runtime = "nodejs18.x"
-
+  runtime          = local.lambdas_common_runtime
 
   environment {
     variables = {
-      TABLE_NAME = var.dynamodb_tables["weight_measurements"]
+      TABLE_NAME = local.weight_measurements_table_name
     }
   }
 
-  layers = [ 
+  layers = [
     aws_lambda_layer_version.common_layer.arn,
+    aws_lambda_layer_version.weight_measurements_shared_layer.arn,
   ]
 
   tags = {
@@ -37,5 +47,6 @@ resource "aws_lambda_function" "weight_measurements_insert_lambda" {
     aws_iam_role_policy_attachment.jowe_api_lambda_dynamodb_crud_role_attachment_measurements,
     aws_lambda_layer_version.common_layer,
     aws_lambda_layer_version.weight_measurements_shared_layer,
+    aws_cloudwatch_log_group.weight_measurements_insert_lambda_log_group,
   ]
 }
