6a5375f69a2c3d7c55701323b62f119b9cf7042f
Andrej <andrej.jagar@gmail.com>
2023-05-26 17:39:06 +0200

    Update of dependency versions (#26)

--- terraform/modules/lambda/layers-weight-shared.tf
+++ terraform/modules/lambda/layers-weight-shared.tf
@@ -8,7 +8,8 @@ resource "aws_lambda_layer_version" "weight_shared_layer" {
   filename   = data.archive_file.weight_shared_layer_zip.output_path
   layer_name = "JoWe-weight-shared-layer"
 
-  compatible_runtimes = [local.lambda_runtimes["nodejs_common_runtime"]]
+  compatible_runtimes      = [local.lambda_runtimes["nodejs_common_runtime"]]
+  compatible_architectures = []
 
   source_code_hash = data.archive_file.weight_shared_layer_zip.output_base64sha256
 
