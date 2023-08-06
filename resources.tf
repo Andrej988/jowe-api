124f73ec310b377d8bb8cca5b4c30fe586206395
andrej988 <andrej.jagar@gmail.com>
2023-08-06 21:19:41 +0200

    Correction by terraform fmt

--- resources.tf
+++ resources.tf
@@ -43,5 +43,5 @@ module "api" {
   api_lambdas_names     = module.lambda.api_lambdas_names
   sns_and_sqs_arns      = module.sns_and_sqs.sns_and_sqs_arns
   sns_and_sqs_policies  = module.sns_and_sqs.sns_and_sqs_policies
-  CORS_ALLOWED_ORIGIN     = var.CORS_ALLOWED_ORIGIN
+  CORS_ALLOWED_ORIGIN   = var.CORS_ALLOWED_ORIGIN
 }
