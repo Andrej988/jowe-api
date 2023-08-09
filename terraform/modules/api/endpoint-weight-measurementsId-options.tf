425eaf63339d1e96a519184fcfc23fc50556de21
Andrej <andrej.jagar@gmail.com>
2023-08-09 22:08:39 +0200

    Added PUT method to CORS allowed methods (#52)

--- terraform/modules/api/endpoint-weight-measurementsId-options.tf
+++ terraform/modules/api/endpoint-weight-measurementsId-options.tf
@@ -54,7 +54,7 @@ resource "aws_api_gateway_integration_response" "weight_measurements_id_integrat
 
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,OPTIONS,POST'",
+    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,OPTIONS,POST,PUT'",
     "method.response.header.Access-Control-Allow-Origin"  = local.cors_access_control_allow_origin_value
   }
 
