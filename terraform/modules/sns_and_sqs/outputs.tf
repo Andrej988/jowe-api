c38523864879baa4e02b2537b265215a88359b63
andrej988 <andrej.jagar@gmail.com>
2023-09-15 00:30:36 +0200

    Fixed issues with deletion of user data / meal recipes

--- terraform/modules/sns_and_sqs/outputs.tf
+++ terraform/modules/sns_and_sqs/outputs.tf
@@ -28,12 +28,3 @@ output "sns_and_sqs" {
     }
   }
 }
-
-output "sns_and_sqs_policies" {
-  description = "Policies for SNS and SQS"
-  value = {
-    sns_delete_user_data_topic_publish             = aws_iam_policy.sns_delete_user_data_topic_publish_policy.arn
-    sqs_weight_measurements_delete_user_data_queue = aws_iam_policy.sqs_weight_measurements_delete_user_data_queue_process_policy.arn
-    sqs_weight_targets_delete_user_data_queue      = aws_iam_policy.sqs_weight_targets_delete_user_data_queue_process_policy.arn
-  }
-}
--- terraform/modules/sns_and_sqs/sns-delete-user-data-topic-subscriptions.tf
+++ terraform/modules/sns_and_sqs/sns-delete-user-data-topic-subscriptions.tf
@@ -19,3 +19,14 @@ resource "aws_sns_topic_subscription" "delete_user_data_weight_targets" {
     aws_sqs_queue.weight_targets_delete_user_data_queue
   ]
 }
+
+resource "aws_sns_topic_subscription" "delete_user_data_meal_recipes" {
+  protocol  = "sqs"
+  topic_arn = aws_sns_topic.delete_user_data.arn
+  endpoint  = aws_sqs_queue.meal_recipes_delete_user_data_queue.arn
+
+  depends_on = [
+    aws_sns_topic.delete_user_data,
+    aws_sqs_queue.meal_recipes_delete_user_data_queue
+  ]
+}
